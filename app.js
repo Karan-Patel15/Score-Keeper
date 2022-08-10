9f73b2ff540650825902b38315892b35d4ba353c
Karan-Patel15 <knpatel215@gmail.com>
2022-08-10 19:02:53 -0400

    Enhanced parts of the UI using Bulma CSS framework

--- app.js
+++ app.js
@@ -9,22 +9,23 @@ const p2Span = document.querySelector("#p2Header");
 const reset = document.querySelector("#reset");
 const dropdown = document.querySelector("#dropdown");
 
-buttonP1.addEventListener("click", function(e) {
+buttonP1.addEventListener("click", function (e) {
     if (!gameOver) {
         if (playerOneScore < playUntil) {
             playerOneScore++;
             p1Span.innerText = `${playerOneScore}`;
-        } 
+        }
         if (playerOneScore === playUntil) {
             gameOver = true;
             p1Span.classList.add("win");
+            p2Span.classList.add("lose");
         }
     }
-    
-    
+
+
 })
 
-buttonP2.addEventListener("click", function(e) {
+buttonP2.addEventListener("click", function (e) {
     if (!gameOver) {
         if (playerTwoScore < playUntil) {
             playerTwoScore++;
@@ -33,23 +34,24 @@ buttonP2.addEventListener("click", function(e) {
         if (playerTwoScore === playUntil) {
             gameOver = true;
             p2Span.classList.add("win");
+            p1Span.classList.add("lose");
         }
     }
-    
-    
+
+
 })
 
-dropdown.addEventListener("change", function(e) {
+dropdown.addEventListener("change", function (e) {
     playUntil = parseInt(dropdown.value);
 })
 
-reset.addEventListener("click", function(e) {
+reset.addEventListener("click", function (e) {
     playUntil = 0;
     playerOneScore = 0;
     playerTwoScore = 0;
     gameOver = false;
     p1Span.innerText = "0";
     p2Span.innerText = "0";
-    p1Span.classList.remove("win");
-    p2Span.classList.remove("win");
+    p1Span.classList.remove("win", "lose");
+    p2Span.classList.remove("win", "lose");
 });
